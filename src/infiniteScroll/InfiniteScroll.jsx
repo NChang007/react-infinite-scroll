76ead0cc24749a1a5b106619c0223ca31f630ac3
Nelson Chang <nelsonc924@gmail.com>
2023-09-29 09:27:04 -0400

    animation is working

--- src/infiniteScroll/InfiniteScroll.jsx
+++ src/infiniteScroll/InfiniteScroll.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './infiniteScroll.css'
+
+
+
+const addScrollAnimation = () => {
+  const scrollers = document.querySelectorAll('.scroller')
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute('data-animated',true)
+    const scrollerInner = scroller.querySelector('.scroller__inner')
+    const scrollerContent = Array.from(scrollerInner.children)
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true)
+      duplicatedItem.setAttribute('aria-hidden', true) //so that the screen reader doent read it twice 
+      scrollerInner.appendChild(duplicatedItem)
+    })
+  })
+}
 
 const InfiniteScroll = () => {
+
+  useEffect(() => {
+    if(!window.matchMedia('(prefers-reduced-motion: reduce)').matches){
+      addScrollAnimation()
+    }
+  },[])
+
   return (
-    <div>InfiniteScroll</div>
+    <div className='scroller'>
+      <ul className='tag-list scroller__inner' >
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>SSG</li>
+        <li>webdev</li>
+        <li>Animation</li>
+        <li>UI/UX</li>
+      </ul>
+    </div>
   )
 }
 
